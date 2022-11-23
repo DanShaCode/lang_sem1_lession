c7384cf7df4bf7022cc8d54e8696d7d142ab5e04
Danis <danshadance@mail.ru>
2022-11-23 13:39:40 +0300

    Середина семинара

--- task1/task1.cs
+++ task1/task1.cs
@@ -1,1 +1,17 @@
-﻿
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine(123);
+Console.WriteLine(4567);
+
+string myName1 = "Bob";
+Console.WriteLine(myName1);
+
+int age = 32;
+Console.WriteLine(age);
+
+int result;
+result = age  * 10;
+Console.WriteLine(result);
+
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(num);
+
