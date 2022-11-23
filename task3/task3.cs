8dfde0cbbc06056c1f05f4681da4926b4e0ea736
Danis <danshadance@mail.ru>
2022-11-23 23:03:44 +0300

    Выполнено task3

---
+++ task3/task3.cs
@@ -0,0 +1,14 @@
+﻿int num1;
+int num2;
+
+num1 = Convert.ToInt32(Console.ReadLine());
+num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 / num2 == num2)
+{
+    Console.WriteLine("Первое введеное число ЯВЛЯЕТСЯ квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое введеное число НЕ ЯВЛЯЕТСЯ квадратом второго");
+}
