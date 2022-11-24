f5459489bbe4210676906f6887cb085f88273ac3
Danis <danshadance@mail.ru>
2022-11-24 09:22:08 +0300

    Выполнено task5

---
+++ task5/task5.cs
@@ -0,0 +1,23 @@
+﻿// Принимаем число N 
+// Выдаем число в промежутке от -N до N
+
+int N;
+N = Convert.ToInt32(Console.ReadLine());
+
+int mN;
+mN = -N;
+int num = 0;
+int count = 0;
+
+for(int i = 0; i <= N * 2; i++)
+{
+    num = mN + count;
+    Console.WriteLine(num);
+    count = count + 1;
+}
+
+// ЗАВЕРШЕНО
+// ПРИШЛОСЬ ПОПАРИТЬСЯ
+
+
+
